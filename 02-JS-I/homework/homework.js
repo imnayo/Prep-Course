c998e8e1800f11fd7d5bbd53a2ca127ff25d56fb
imnayo <nfcm1997@gmail.com>
2022-12-22 09:52:57 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "que paso";
+const nuevoNum = "hola";
 
 // Crea una variable booleana:
 const nuevoBool = null;
